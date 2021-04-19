bec09d29d30b7b3656d2b3bbb3ed739ee096d4f8
Aktan-web <idinex@gk.ru>
2021-04-19 23:09:43 +0600

    added module grappelli for admin interface

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('index/', include('polls.urls')),
+    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
 ]
